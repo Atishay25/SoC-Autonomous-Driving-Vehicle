56b552a1dc48265fba90ed0da6d08cfbb3c51514
Atishay25 <atishayjain2552@gmail.com>
2023-06-16 23:47:31 +0530

    via matrix mult

--- markov_chains/markov_chain.py
+++ markov_chains/markov_chain.py
@@ -1,15 +1,42 @@
 import numpy as np
 
+def multiply(A,B,n):
+    C = np.zeros((n,n), float)
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                C[i][j] += A[i][k] * B[k][j]
+    return C
+
+
 n = int(input())    # number of states
 initial_state = int(input())
 final_state = int(input())
-T = int(input())
-M = list()
+t = int(input())
+m = list()
 
 for i in range(n):
     l = list(map(float, input().split()))
-    M = M + l
+    m = m + l
 
-matrix = np.array(M).reshape(n,n)
+p = np.array(m).reshape(n,n)
 
-print(matrix)
+P_t = np.zeros((n,n), float)
+t1 = t
+
+for i in range(n):
+    P_t[i][i] = 1
+
+while(t > 0):
+    if t%2 != 0:
+        P_t = multiply(P_t, p, n)
+    p = multiply(p,p,n)
+    t = t // 2
+    #P_t = multiply(P_t,p,n)
+    #t -= 1
+
+ans = "Probability of reaching state " + str(final_state) + " at time t = " + str(t1) + " starting from state " + str(initial_state) + " at t = 0 is : "
+print(ans,P_t[initial_state][final_state])
+
+
+
